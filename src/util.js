a0ef93c5aca6adc986e01e46d31d936fc0c4baa6
Breno Xavier <brenoxav@gmail.com>
2021-08-05 11:53:13 -0400

    Sort scores list

--- src/util.js
+++ src/util.js
@@ -4,14 +4,21 @@ const scoreList = document.querySelector('.score-list');
 const inputName = document.querySelector('.input-name');
 const inputScore = document.querySelector('.input-score');
 
+const compareScores = (a, b) => {
+  return (a.score > b.score) ? -1 : ((a.score < b.score) ? 1 : 0);
+}
+
 const renderList = () => {
   scoreList.innerHTML = '';
   myGame.getScores()
     .then((scores) => {
-      scores.result.forEach((score, index) => {
+      const scoresArr = scores.result;
+      scoresArr.sort(compareScores);
+      scoresArr.forEach((score, index) => {
         scoreList.innerHTML += `
           <li class="score-item${index % 2 === 0 ? ' item-shade' : ''}">
-            ${score.user}: ${score.score}
+            <span class="item-user">${score.user}</span>
+            <span class="item-score">${score.score}</span>
           </li>
         `;
       });
